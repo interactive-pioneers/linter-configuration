3ba7f719893e0b7f73222c180e44157912eeb410
Michelle Peters <mpe@interactive-pioneers.de>
2018-02-06 12:47:24 +0100

    test

--- postinstall.sh
+++ postinstall.sh
@@ -1,9 +1,1 @@
-#!/bin/bash
-
-# go to YOUR_NEEDED_DIRECTORY .e.g "dist"
-#cd YOUR_NEEDED_DIRECTORY
-
-# copy each file/dir to user dir(~/)
-ls -la ../../
 cp -R files/.htmlhintrc ../../
-echo 'robin'
