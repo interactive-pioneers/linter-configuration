c9ad639156e7c8c4bffdc00a36ffee1836902ec1
Michelle Peters <mpe@interactive-pioneers.de>
2018-02-06 12:36:33 +0100

    add postinstall script

---
+++ postinstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# go to YOUR_NEEDED_DIRECTORY .e.g "dist"
+#cd YOUR_NEEDED_DIRECTORY
+
+# copy each file/dir to user dir(~/)
+cp -R node_modules/ip-linter-configuration/files/.htmlhintrc .
+echo 'robin'
