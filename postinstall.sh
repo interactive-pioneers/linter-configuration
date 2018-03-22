c847cae83e9dd46b7b62bdeb5ef34a3e11005569
Michelle Peters <mpe@interactive-pioneers.de>
2018-03-22 14:07:29 +0100

    fix bug

--- postinstall.sh
+++ postinstall.sh
@@ -1,19 +1,19 @@
-# if ! [ -f ../../.htmlhintrc ]
-# then
-# cp files/.htmlhintrc ../../.htmlhintrc
-# fi
-#
-# if ! [ -f ../../.jscsrc ]
-# then
-# cp files/.jscsrc ../../.jscsrc
-# fi
-#
-# if ! [ -f ../../.jshintrc ]
-# then
-# cp files/.jshintrc ../../.jshintrc
-# fi
-#
-# if ! [ -f ../../.sass-lint.yml ]
-# then
-# cp files/.sass-lint.yml ../../.sass-lint.yml
-# fi
+if ! [ -f ../../.htmlhintrc ]
+then
+cp files/.htmlhintrc ../../.htmlhintrc
+fi
+
+if ! [ -f ../../.jscsrc ]
+then
+cp files/.jscsrc ../../.jscsrc
+fi
+
+if ! [ -f ../../.jshintrc ]
+then
+cp files/.jshintrc ../../.jshintrc
+fi
+
+if ! [ -f ../../.sass-lint.yml ]
+then
+cp files/.sass-lint.yml ../../.sass-lint.yml
+fi
