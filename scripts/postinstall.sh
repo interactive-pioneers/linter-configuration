6963dd2b495b13850fad2eabfa4d8511906163e2
Michelle Peters <mpe@interactive-pioneers.de>
2018-03-22 11:36:35 +0100

    update postscript

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 #copy linter files to project folder
 
+if ! [ -e ../../.htmlhintrc ]
+then
 cp -u files/.htmlhintrc ../../.htmlhintrc
+fi
+
+if ! [ -e ../../.jscsrc ]
+then
 cp -u files/.jscsrc ../../.jscsrc
+fi
+
+if ! [ -e ../../.jshintrc ]
+then
 cp -u files/.jshintrc ../../.jshintrc
+fi
+
+if ! [ -e ../../.sass-lint.yml ]
+then
 cp -u files/.sass-lint.yml ../../.sass-lint.yml
