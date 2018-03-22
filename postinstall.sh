f6f100ba83a2ff12105b8355d1c0248695687c3b
Michelle Peters <mpe@interactive-pioneers.de>
2018-03-22 13:55:19 +0100

    test

--- postinstall.sh
+++ postinstall.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-if ! [ -f ../../.htmlhintrc ]
-then
-cp files/.htmlhintrc ../../.htmlhintrc
-fi
-
-if ! [ -f ../../.jscsrc ]
-then
-cp files/.jscsrc ../../.jscsrc
-fi
-
-if ! [ -f ../../.jshintrc ]
-then
-cp files/.jshintrc ../../.jshintrc
-fi
-
-if ! [ -f ../../.sass-lint.yml ]
-then
-cp files/.sass-lint.yml ../../.sass-lint.yml
-fi
+# if ! [ -f ../../.htmlhintrc ]
+# then
+# cp files/.htmlhintrc ../../.htmlhintrc
+# fi
+#
+# if ! [ -f ../../.jscsrc ]
+# then
+# cp files/.jscsrc ../../.jscsrc
+# fi
+#
+# if ! [ -f ../../.jshintrc ]
+# then
+# cp files/.jshintrc ../../.jshintrc
+# fi
+#
+# if ! [ -f ../../.sass-lint.yml ]
+# then
+# cp files/.sass-lint.yml ../../.sass-lint.yml
+# fi
